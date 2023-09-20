257b8e0d86274b59488b486d537ea6fbb763f9cf
ankitsharma221 <aannkkiitt321@gmail.com>
2023-09-20 09:07:58 +0530

    done
    commit 1

--- lib/main.dart
+++ lib/main.dart
@@ -8,20 +8,35 @@ import 'package:flutter/material.dart';
 import 'screens/farmer_site/register_login/farmer_dashboard.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+  runApp(MaterialApp(
+    title: 'Flutter Demo',
+
+    theme: ThemeData(
+      primaryColor: Colors.green,
+      hintColor: Colors.greenAccent,
+      fontFamily: 'Roboto',
+      appBarTheme: AppBarTheme(
+        color: Colors.green,
+        elevation: 0, // Remove app bar shadow
       ),
-      initialRoute: 'RoleScreen',
-      routes: {
-        'RoleScreen': (context) => RoleSelectionScreen(),
-        'FarmerLogin': (context) => FarmerLogin(),
-        'Navbar': (context) => NavBar(),
-        'FarmerDash': (context) => FarmerDash(),
-        'CameraScreen': (context) => CameraScreen(),
-      },
-    ),
-  );
+      textTheme: TextTheme(
+        bodyText2: TextStyle(
+          fontSize: 16.0,
+          color: Colors.black87, // Adjust text color
+        ),
+      ),
+      elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+        primary: Colors.green,
+      )),
+    ), // Change button color
+    initialRoute: 'RoleScreen',
+    routes: {
+      'RoleScreen': (context) => RoleSelectionScreen(),
+      'FarmerLogin': (context) => FarmerLogin(),
+      'Navbar': (context) => NavBar(),
+      'FarmerDash': (context) => FarmerDash(),
+      'CameraScreen': (context) => CameraScreen(),
+    },
+  ));
 }
--- lib/screens/farmer_site/register_login/camera_screen.dart
+++ lib/screens/farmer_site/register_login/camera_screen.dart
@@ -1,6 +1,20 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:camera/camera.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:audioplayers/audioplayers.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: CameraScreen(),
+    );
+  }
+}
 
 class CameraScreen extends StatefulWidget {
   @override
@@ -8,108 +22,228 @@ class CameraScreen extends StatefulWidget {
 }
 
 class _CameraScreenState extends State<CameraScreen> {
-  late CameraController _cameraController;
-  bool isCameraVisible = false;
-  List<CameraDescription>? cameras;
+  File? _imageFile;
+  final picker = ImagePicker();
+  bool _showLogo = true;
 
-  @override
-  void initState() {
-    super.initState();
-    // Initialize the camera
-    availableCameras().then((cameraList) {
-      cameras = cameraList;
-      if (cameras != null && cameras!.isNotEmpty) {
-        _cameraController = CameraController(
-          cameras![0], // Use the first available camera
-          ResolutionPreset.medium,
-        );
-        initializeCamera();
-      }
-    });
-  }
+  Future<void> _takePicture() async {
+    final pickedImage = await picker.pickImage(source: ImageSource.camera);
 
-  Future<void> initializeCamera() async {
-    await _cameraController.initialize();
-    if (mounted) {
-      setState(() {
-        isCameraVisible = true;
-      });
+    if (pickedImage != null) {
+      _navigateToDescriptionScreen(File(pickedImage.path));
     }
   }
 
-  @override
-  void dispose() {
-    _cameraController.dispose();
-    super.dispose();
-  }
+  Future<void> _openGallery() async {
+    final pickedImage = await picker.pickImage(source: ImageSource.gallery);
 
-  void toggleCameraVisibility() {
-    setState(() {
-      isCameraVisible = !isCameraVisible;
-    });
-  }
-
-  void _takePhoto() async {
-    // Implement logic to take a photo using the camera
-    if (_cameraController != null && _cameraController.value.isInitialized) {
-      XFile? photo = await _cameraController.takePicture();
-      // Implement navigation or other actions with the taken photo
+    if (pickedImage != null) {
+      _navigateToDescriptionScreen(File(pickedImage.path));
     }
   }
 
-  void _pickFromGallery() async {
-    // Implement logic to pick an image or video from the gallery
-    final picker = ImagePicker();
-    XFile? pickedFile = await picker.pickImage(source: ImageSource.gallery);
-    // Implement navigation or other actions with the picked file
+  void _navigateToDescriptionScreen(File imageFile) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => ImageDescriptionScreen(imageFile),
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Camera Options'),
+        shadowColor: Colors.transparent,
+        backgroundColor: Colors.transparent,
       ),
-      body: Stack(
-        children: [
-          // Background Image or Gradient
-          Container(
-            width: double.infinity,
-            height: double.infinity,
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage(
-                    'assets/background_image.jpg'), // Replace with your image
-                fit: BoxFit.cover,
-              ),
-            ),
-            child: isCameraVisible
-                ? Column(
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: <Widget>[
-                      Expanded(
-                        child: CameraPreview(_cameraController),
-                      ),
-                      ElevatedButton(
-                        onPressed: () {
-                          _takePhoto();
-                        },
-                        child: Text('Take Photo'),
-                      ),
-                      // Add more buttons or UI elements here
-                    ],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            _showLogo
+                ? Image.asset(
+                    'assets/images/plant.png', // Replace with your image path
+                    height: 200,
+                    width: 200.0,
                   )
-                : Center(
-                    child: ElevatedButton(
-                      onPressed: () {
-                        toggleCameraVisibility();
-                      },
-                      child: Text('Show Camera'),
+                : Container(),
+            _imageFile == null
+                ? Container()
+                : Image.file(
+                    _imageFile!,
+                    height: 200.0,
+                  ),
+            SizedBox(height: 16.0),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  width: 160.0,
+                  height: 50.0,
+                  child: ElevatedButton.icon(
+                    onPressed: _takePicture,
+                    icon: Icon(Icons.camera),
+                    label: Text('Take Picture'),
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.blue,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
                     ),
                   ),
+                ),
+                SizedBox(height: 16.0),
+                Container(
+                  width: 160.0,
+                  height: 50.0,
+                  child: ElevatedButton.icon(
+                    onPressed: _openGallery,
+                    icon: Icon(Icons.photo),
+                    label: Text('Open Gallery'),
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.green,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ImageDescriptionScreen extends StatefulWidget {
+  final File imageFile;
+
+  ImageDescriptionScreen(this.imageFile);
+
+  @override
+  _ImageDescriptionScreenState createState() => _ImageDescriptionScreenState();
+}
+
+class _ImageDescriptionScreenState extends State<ImageDescriptionScreen> {
+  AudioPlayer audioPlayer = AudioPlayer();
+  bool isRecording = false;
+  String recordedAudioPath = ''; // Store the recorded audio file path
+
+  @override
+  void dispose() {
+    audioPlayer.dispose();
+    super.dispose();
+  }
+
+  Future<void> _toggleRecording() async {
+    if (!isRecording) {
+      // Start recording audio
+      String tempPath = 'path_to_temp_audio_file'; // Replace with actual path
+      // You can use a package like audioplayers_record to record audio.
+      // Make sure to import and follow its documentation.
+      // Example:
+      // await audioRecorder.start(path: tempPath, audioOutputFormat: AudioOutputFormat.AAC);
+      setState(() {
+        isRecording = true;
+        recordedAudioPath = tempPath;
+      });
+    } else {
+      // Stop recording audio
+      // Example:
+      // await audioRecorder.stop();
+      setState(() {
+        isRecording = false;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Image Description'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Image.file(widget.imageFile, height: 200.0),
+          SizedBox(height: 16.0),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TextFormField(
+              decoration: InputDecoration(
+                labelText: 'Description',
+                hintText: 'Enter a detailed description...',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                contentPadding: EdgeInsets.symmetric(
+                  vertical: 20.0,
+                  horizontal: 16.0,
+                ),
+                suffixIcon: isRecording
+                    ? IconButton(
+                        icon: Icon(Icons.stop),
+                        onPressed: () {
+                          _toggleRecording();
+                        },
+                      )
+                    : IconButton(
+                        icon: Icon(Icons.mic),
+                        onPressed: () {
+                          _toggleRecording();
+                        },
+                      ),
+              ),
+              maxLines: 4,
+              // Add recorded audio to the description
+              initialValue: recordedAudioPath,
+            ),
           ),
+          SizedBox(height: 16.0),
+          if (recordedAudioPath.isNotEmpty)
+            Column(
+              children: [
+                Text('Recorded Audio:'),
+                // Display the audio player for the recorded audio
+                AudioPlayerWidget(recordedAudioPath),
+              ],
+            ),
         ],
       ),
+    );
+  }
+}
+
+class AudioPlayerWidget extends StatelessWidget {
+  final String audioFilePath;
+
+  var AudioSource;
+
+  AudioPlayerWidget(this.audioFilePath);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        ElevatedButton(
+          onPressed: () {
+            // Create a new AudioPlayer object
+            final audioPlayer = AudioPlayer();
+
+            // Convert the audio file path to a Source object
+            final source = AudioSource.uri(audioFilePath, isLocal: true);
+
+            // Play recorded audio
+            audioPlayer.play(source);
+          },
+          child: Text('Play Recorded Audio'),
+        ),
+      ],
     );
   }
 }
